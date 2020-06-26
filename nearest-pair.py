7d17c78836b13aa4f5b8b048ac5c2dfe64065a23
Ackerley Cheng <dacker35@gmail.com>
2020-06-26 16:40:27 +0800

    not completed.

--- nearest-pair.py
+++ nearest-pair.py
@@ -4,6 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import random as rd
 import math
+import sys
 
 
 '''
@@ -31,6 +32,13 @@ def nearest_pair(points):
     '''
     The input points should be sorted by x axis in ascending order.
     '''
+    if len(points) <= 1:
+        return sys.maxsize
+    elif len(points) == 2:
+        x1, y1 = points[0]
+        x2, y2 = points[1]
+        return math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
+    
     mid = int(len(points) / 2)
     L = points[:mid]
     R = points[mid:]
@@ -60,23 +68,24 @@ def nearest_pair(points):
         xi, yi, side_i = point_i
         if side_i == 'L':
             for j in range(i-1, -1, -1):
-                xj, yj, side_j = points[j]
+                xj, yj, side_j = bar[j]
                 distx_ij = xi - xj
                 if distx_ij > d:
                     break
                 elif side_j == 'R':
-                    dist_ij = math.sqrt(math.pow(xi-xj, 2), math.pow(yi-yj, 2))
+                    dist_ij = math.sqrt(math.pow(xi-xj, 2) + math.pow(yi-yj, 2))
                     d = min([d, dist_ij])
-            for j in range(i+1, len(points), 1):
-                xj, yj, side_j = points[j]
+            for j in range(i+1, len(bar), 1):
+                xj, yj, side_j = bar[j]
                 distx_ij = xj - xi
                 if distx_ij > d:
                     break
                 elif side_j == 'R':
-                    dist_ij = math.sqrt(math.pow(xj-xi, 2), math.pow(yj-yi, 2))
+                    dist_ij = math.sqrt(math.pow(xj-xi, 2) + math.pow(yj-yi, 2))
                     d = min([d, dist_ij])
-                    
-                    
+    
+    return d                
+
 
 
 # Generate a set of points randomly
@@ -85,3 +94,7 @@ points = randPoints(21)
 # Draw the input set of points
 plotPoints(points, 'bo')
 plt.show()
+
+min_dist = nearest_pair(points)
+print('minimum distance: ', min_dist)
+
